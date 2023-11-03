90adb924ecce5dd67079df3677efac7cdc254ff3
SubhajitD <subhajitdebnath.999@gmail.com>
2023-11-03 22:56:31 +0530

    Movies fetch from TMDB

--- src/components/Body.js
+++ src/components/Body.js
@@ -26,7 +26,7 @@ const Body = () => {
     useEffect(() => {
 
         onAuthStateChanged(auth, (user) => {
-            console.log(user)
+            // console.log(user)
             if (user) {
                 // User is signed in, see docs for a list of available properties
                 // https://firebase.google.com/docs/reference/js/auth.user
--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from './Header';
+import { API_OPTIONS } from '../utils/constant';
 
 const Browse = () => {
+
+  const getNowPlayingMovies = async() => {
+    const data = await fetch("https://api.themoviedb.org/3/discover/movie?include_adult=true&include_video=true&language=en-US&page=1&sort_by=popularity.desc", API_OPTIONS);
+    const json = await data.json();
+    console.log(json.results);
+  }
+
+  useEffect(() => {
+    getNowPlayingMovies();
+  }, []);
+
   return (
     <div>
       <Header />
