361ba00c38557537f99c65ce8eded1c7ddc6cce6
SubhajitD <subhajitdebnath.999@gmail.com>
2023-11-09 18:36:19 +0530

    update css

--- src/components/post-auth/browse/MovieCard.js
+++ src/components/post-auth/browse/MovieCard.js
@@ -4,7 +4,7 @@ import { IMG_CDN } from '../../../utils/constant';
 const MovieCard = (movie) => {
     console.log(movie?.movie);
   return (
-    <div className="w-36 md:w-48 pr-4">
+    <div className="w-36 md:w-48 pr-4 hover:cursor-pointer">
       <img alt="Movie Card" src={IMG_CDN + movie?.movie?.poster_path} />
     </div>
   )
--- src/components/post-auth/browse/MovieListContainer.js
+++ src/components/post-auth/browse/MovieListContainer.js
@@ -10,8 +10,19 @@ const MovieListContainer = () => {
   return (
     <div className="bg-black">
       <div className=" mt-0 md:-mt-52 pl-4 md:pl-12 relative z-20">
-        <div className="px-6 ">
+        <div className="px-6">
           <h1 className="text-lg md:text-3xl py-4 text-white">Now Playing</h1>
+          <div className="flex overflow-x-scroll">
+            <div className="flex">
+              {movies?.map((movie) => (
+                <MovieCard key={movie.id} movie={movie} />
+              ))}
+            </div>
+          </div>
+        </div>
+
+        <div className="px-6">
+          <h1 className="text-lg md:text-3xl py-4 text-white">Recent Movies</h1>
           <div className="flex overflow-x-scroll">
             <div className="flex">
               {movies?.map((movie) => (
