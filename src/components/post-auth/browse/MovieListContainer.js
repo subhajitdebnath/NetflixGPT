92014cb6457bb875059284687665d8e4fcb1fd9c
SubhajitD <subhajitdebnath.999@gmail.com>
2023-11-13 22:22:04 +0530

    movie suggestion

--- src/components/post-auth/browse/MovieListContainer.js
+++ src/components/post-auth/browse/MovieListContainer.js
@@ -31,6 +31,17 @@ const MovieListContainer = () => {
             </div>
           </div>
         </div>
+
+        <div className="px-6">
+          <h1 className="text-lg md:text-3xl py-4 text-white">Movie Suggestions</h1>
+          <div className="flex overflow-x-scroll">
+            <div className="flex">
+              {movies?.map((movie) => (
+                <MovieCard key={movie.id} movie={movie} />
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
