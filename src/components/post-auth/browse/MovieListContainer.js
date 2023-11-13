7f9b0e8a1f30fbe068e7d4ae7c4d0127738e5a0f
SubhajitD <subhajitdebnath.999@gmail.com>
2023-11-13 22:21:20 +0530

    update

--- src/components/post-auth/browse/MovieListContainer.js
+++ src/components/post-auth/browse/MovieListContainer.js
@@ -22,7 +22,7 @@ const MovieListContainer = () => {
         </div>
 
         <div className="px-6">
-          <h1 className="text-lg md:text-3xl py-4 text-white">Recent Movies</h1>
+          <h1 className="text-lg md:text-3xl py-4 text-white">Recent Playing</h1>
           <div className="flex overflow-x-scroll">
             <div className="flex">
               {movies?.map((movie) => (
